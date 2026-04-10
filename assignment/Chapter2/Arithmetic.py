ccb4c0ebe05a7b85365eec285ed14f2d7271ab83
Vincent Opurum <vincentchimene@gmail.com>
2026-04-10 23:59:54 +0100

    adds completed tasks

---
+++ assignment/Chapter2/Arithmetic.py
@@ -0,0 +1,21 @@
+"""
+2.4 (Arithmetic)
+For each of the arithmetic operators +, -, *, /, // and **, display the value of an expression with 27.5 as the left operand and 2 as the right operand.
+
++   29.5
+-   25.5
+*   55.0
+/   13.75
+//  13.0
+**  756.25
+"""
+
+number1 = 27.5
+number2 = 2
+
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+print(number1 // number2)
+print(number1 ** number2)
---
+++ assignment/Chapter2/Circle.py
@@ -0,0 +1,14 @@
+"""
+2.5 (Circle Area, Diameter and Circumference)
+For a circle of radius 2, display the diameter, circumference and area. Use the value 3.14159 for π. Use the following formulas (r is the radius): diameter = 2r, circumference = 2πr and area = πr².
+"""
+
+radius = 2
+pi = 3.14159
+
+diameter = 2 * radius
+circumference = 2 * pi * radius
+area = pi * radius ** 2
+print('diameter = ',diameter)
+print('cicumference = ',circumference)
+print('area = ',area)
