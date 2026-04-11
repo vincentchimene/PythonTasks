91a61abc6435e96a9f5fecd759c993214960ea24
Vincent Opurum <vincentchimene@gmail.com>
2026-04-11 01:52:01 +0100

    adds 3 more tasks

---
+++ assignment/Chapter2/Multiples.py
@@ -0,0 +1,11 @@
+"""
+2.7 (Multiples)
+Use if statements to determine whether 1024 is a multiple of 4 and whether 2 is a multiple of 10.
+Hint: Use the remainder operator.
+"""
+
+if 1024 % 4 == 0:
+    print("1024 is a multiple of 4")
+if 10 % 2 == 0:
+    print("10 is a multiple of 2")
+    
---
+++ assignment/Chapter2/square_And_Cube.py
@@ -0,0 +1,30 @@
+"""
+2.8 (Table of Squares and Cubes)
+Write a script that calculates the squares and cubes of the numbers from 0 to 5. Print the resulting values in table format, as shown below. Use the tab escape sequence to achieve the three-column output.
+
+number square cube
+0 0 0
+1 1 1
+2 4 8
+3 9 27
+4 16 64
+5 25 125
+"""
+print("number\tsquare\tcube")
+number = 0
+print(number,"\t",number ** 2,"\t",number ** 3)
+
+number = number + 1
+print(number,"\t",number ** 2,"\t",number ** 3)
+
+number = number + 1
+print(number,"\t",number ** 2,"\t",number ** 3)
+
+number = number + 1
+print(number,"\t",number ** 2,"\t",number ** 3)
+
+number = number + 1
+print(number,"\t",number ** 2,"\t",number ** 3)
+
+number = number + 1
+print(number,"\t",number ** 2,"\t",number ** 3)
