1ec3a172b8a2fef3b4539b9987972cdb91c9fc63
Vincent Opurum <vincentchimene@gmail.com>
2026-04-20 11:24:16 +0100

    adds number triangle

---
+++ classWork/number_triangle.py
@@ -0,0 +1,12 @@
+"""
+
+"""
+
+row = int (input("Enter number"))       
+while row >0:
+    column = row
+    while column > 0:
+        print(column, end='')
+        column = column - 1
+    print()
+    row = row - 1
---
+++ classWork/timesTable.py
@@ -0,0 +1,9 @@
+"""
+Take number
+repeat multiplication of number with count
+print result
+
+"""
+number = int (input("Enter number"))
+for count in range(1, 11):
+        print(number," X ",count, " = ", number*count)
