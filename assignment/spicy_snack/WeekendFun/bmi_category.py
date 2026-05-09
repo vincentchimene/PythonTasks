5fb283f0f3f8e2b5202947b1a45ebb68ad7a0ce9
Vincent Opurum <vincentchimene@gmail.com>
2026-05-09 11:54:21 +0100

    adds bmi module

---
+++ assignment/spicy_snack/WeekendFun/bmi_category.py
@@ -0,0 +1,34 @@
+#QUESTION
+#BMI Category : Ask for weight (kg) and height (meters).
+#Compute bmi = weight / (height * height) Then print:
+#
+#< 18.5 → "Underweight"
+#between 18.5 and 24.9 → "Normal"
+#between 25 and 29.9 → "Overweight"
+#>= 30 → "Obese"
+
+
+#PSEUDOCODE
+#collect weight and height
+#compute bmi = weight / (height * height)
+#compare bmi and return:
+#   < 18.5 → "Underweight"
+#   between 18.5 and 24.9 → "Normal"
+#   between 25 and 29.9 → "Overweight"
+#   >= 30 → "Obese"
+#call function
+#print
+
+
+
+def get_bmi(weight, height):
+    bmi = weight / (height*height)
+    if bmi < 18.5
+        result = "Underweight"
+        
+//STILL ON IT
+    
+    
+    
+
+
